3bd045bb2c1b5347e0116273682ca96df901b811
Parvin khan <58837691+parveen11498@users.noreply.github.com>
2022-04-08 16:46:45 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import './index.css';
-import App from "./App";
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-
-
-
-ReactDom.render(<App/>,document.getElementById("cart"));
