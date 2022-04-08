dbb9b332a6fbad5e289bdcc615ff16a0105ec304
Parvin khan <58837691+parveen11498@users.noreply.github.com>
2022-04-08 16:44:03 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from "./App";
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
+
+
+ReactDom.render(<App/>,document.getElementById("cart"));
